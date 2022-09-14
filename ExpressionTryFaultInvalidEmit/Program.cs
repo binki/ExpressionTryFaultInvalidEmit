4b92b822091f50bf73f2ec64f45d7e022983b24b
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2022-09-14 19:35:49 -0400

    Initial.

---
+++ ExpressionTryFaultInvalidEmit/Program.cs
@@ -0,0 +1,4 @@
+﻿using System;
+using System.Linq.Expressions;
+
+Console.WriteLine(Expression.Lambda<Func<string>>(Expression.TryFault(Expression.Constant("hi"), Expression.Constant("alternative"))).Compile()());
